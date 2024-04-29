13bd83b608588f8e89ce02f29e6bcdcd192a1eec
ayabakry <ayatbakry@yahoo.com>
2024-04-29 21:55:21 +0300

    add super saving text section

--- src/Component/DiscountSection.jsx
+++ src/Component/DiscountSection.jsx
@@ -1,9 +1,16 @@
 import Sofa from "../Imgs/SofaDiscount.png"
 function DiscountSection (){
     return(
-    <div className="grid grid-cols-3 gap-4">
-<img src={Sofa} className="col-span-2 ml-60"/>
-        <h1 className="mt-40 uppercase ">Super Saving</h1>
+    <div className="grid grid-cols-3">
+        <img src={Sofa} className="col-span-2 ml-60"/>
+        <div>
+        <h1 className="mt-40 ">Super Saving </h1>
+        <p className=" ">Sale Up to 40% off +12 months Financing</p>
+        <p className=" ">Create a room that's welcoming with a sofa or a section couach.</p>
+        <p className="">Tp help you go the extra mile for comfort and style</p>
+        
+         <button className="bg-black text-white px-10 py-2 mt-8 rounded">Shop Now</button>
+         </div>
     </div>
     
     
