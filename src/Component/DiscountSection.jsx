79abbda2a46291cbdea2f4ccecc73a9499b709f1
ayabakry <ayatbakry@yahoo.com>
2024-04-29 22:49:34 +0300

    ..

--- src/Component/DiscountSection.jsx
+++ src/Component/DiscountSection.jsx
@@ -1,13 +1,13 @@
 import Sofa from "../Imgs/SofaDiscount.png"
 function DiscountSection (){
     return(
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-3" >
         <img src={Sofa} className="col-span-2 ml-60"/>
-        <div>
-        <h1 className="mt-40 text-xl uppercase ">Super <span className="text-[#142B7B] text-base">Saving </span></h1>
-        <p className="text-[#142B7B]">Sale Up to 40% off +12 months Financing</p>
-        <p className=" ">Create a room that's welcoming with a sofa or a section couach.</p>
-        <p className="">Tp help you go the extra mile for comfort and style</p>
+        <div className="">
+        <h1 className="mt-40 text-2xl uppercase ">Super <span className="text-[#142B7B] text-base">Saving </span></h1>
+        <p className="text-[#142B7B] text-lg">Sale Up to 40% off +12 months Financing</p>
+        <p className="text-sm ">Create a room that's welcoming with a sofa or a section couach.</p>
+        <p className="text-sm">Tp help you go the extra mile for comfort and style</p>
         
          <button className="bg-black text-white px-10 py-2 mt-8 rounded">Shop Now</button>
          </div>
